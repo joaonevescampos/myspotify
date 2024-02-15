8db3bbff49f947dfa2a1bb24dc4edbb1c140d80a
João Campos <joaon.c.jv@gmail.com>
2024-02-14 23:26:10 -0300

    Inserted the project cover musics

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -7,6 +7,10 @@ import card1 from '../../assets/cards/5.jpeg'
 import card2 from '../../assets/cards/6.jpeg'
 import card3 from '../../assets/cards/7.jpeg'
 import card4 from '../../assets/cards/8.jpeg'
+import card5 from '../../assets/cards/9.jpeg'
+import card6 from '../../assets/cards/10.jpeg'
+import card7 from '../../assets/cards/11.jpeg'
+import card8 from '../../assets/cards/12.jpeg'
 
 const Main = () => {
     return ( 
@@ -20,7 +24,7 @@ const Main = () => {
                     <img src={album4} alt='album4' />
                 </div>
                 <h1>Categorias</h1>
-                <div className={style.albuns} >
+                <div className={style.cards} >
                     <div className={style.card} >Funk</div>
                     <div className={style.card} >Funk</div>
                     <div className={style.card} >Funk</div>
@@ -28,11 +32,19 @@ const Main = () => {
                 </div>
                 
                 <h1>Recomendado</h1>
-                <div className={style.albuns} >
+                <div className={style.covers} >
                     <img src={card1} alt='card1' />
                     <img src={card2} alt='card2' />
                     <img src={card3} alt='card3' />
                     <img src={card4} alt='card4' />
+                </div>
+
+                <h1>Favoritos</h1>
+                <div className={style.covers} >
+                    <img src={card5} alt='card5' />
+                    <img src={card6} alt='card6' />
+                    <img src={card7} alt='card7' />
+                    <img src={card8} alt='card8' />
                 </div>
             </div>
         </main>
